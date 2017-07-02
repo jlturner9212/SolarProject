7e8147de580b776541da048a8d08bdc5fe3eb561
Jeff Turner <jeff.turner@rainforestdigital.com>
2017-07-02 19:41:37 -0400

    init commit

---
+++ Assets/Jupiter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jupiter : Planetoid {
+
+	// Use this for initialization
+	void Start()
+	{
+		Setup();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Run();
+	}
+}
---
+++ Assets/Uranus.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Uranus : Planetoid {
+
+	// Use this for initialization
+	void Start()
+	{
+		Setup();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Run();
+	}
+}
